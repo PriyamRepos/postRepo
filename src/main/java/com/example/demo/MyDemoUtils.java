e60c3152d487dd8fd463f33a0509163717e09631
Priyam <priyamchoudhury1996@gmail.com>
2024-04-18 17:36:55 +0530

    added new file

---
+++ src/main/java/com/example/demo/MyDemoUtils.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class MyDemoUtils {
+
+	public String demo() {
+		System.out.println("Hello World");
+		return "";
+	}
+	
+	public void m2() {
+		
+		System.out.println("Hello World");
+	}
+}
